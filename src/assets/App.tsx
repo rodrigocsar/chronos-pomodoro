ebf0d778a3a7f64b0d05f3af5943290c2f0e6f85
rodrigocsar <rodrigocesar068@gmail.com>
2026-01-19 08:18:34 +0044

    css module

---
+++ src/assets/App.tsx
@@ -0,0 +1,16 @@
+import Heading from "../components/Heading";
+
+export default function App() {
+  return (
+    <div>
+      <Heading />
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nemo
+        atque minima voluptate mollitia. Minus iusto non pariatur, doloremque,
+        praesentium similique dolor voluptate consectetur impedit, a ab
+        provident culpa necessitatibus.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Heading.tsx
@@ -0,0 +1,5 @@
+import styles from "./Heading.module.css";
+
+export default function Heading() {
+  return <h1 className={styles.heading}> Olá mundo </h1>;
+}
